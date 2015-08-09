2722811dcf650c96f6622d6205b3f09977972203
Bartosz Kostaniak <bartoszkostaniak@gmail.com>
2015-08-09 19:18:23 +0200

    Add project model

---
+++ src/model/CProjectModel.cpp
@@ -0,0 +1,5 @@
+#include "CProjectModel.hpp"
+
+CProjectModel::CProjectModel()
+{
+}
---
+++ src/model/CProjectModel.hpp
@@ -0,0 +1,12 @@
+#ifndef CPROJECTMODEL_HPP
+#define CPROJECTMODEL_HPP
+
+#include <QAbstractTableModel>
+
+class CProjectModel : public QAbstractTableModel
+{
+    public:
+        CProjectModel();
+};
+
+#endif // CPROJECTMODEL_HPP
