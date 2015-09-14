b0a924e2b4e913600d3132272564717e49676ebd
Bartosz Kostaniak <bartoszkostaniak@gmail.com>
2015-09-14 19:44:06 +0200

    Project types in a form of a tree view (read from directory ~/.projectizer)

--- src/file_finder/CProjectTemplateFileFinder.cpp
+++ src/file_finder/CProjectTemplateFileFinder.cpp
@@ -1,4 +1,5 @@
 #include "CProjectTemplateFileFinder.hpp"
+#include "src/parser/CProjectTypeXmlFileReader.hpp"
 #include "src/new_projects/model/CProjectTypeItem.hpp"
 
 #include <QList>
@@ -15,19 +16,24 @@ CProjectTypeItem *CProjectTemplateFileFinder::findTemplateFilesBFS(CProjectTypeI
             QDir directory(entries.front().first.absoluteFilePath(), "", QDir::Name);
             CProjectTypeItem *current_parent = entries.front().second;
             entries.pop_front();
-            
+
             for (QFileInfo entry : directory.entryInfoList(QDir::NoDotAndDotDot | QDir::Dirs)) {
-                SProjectTypeInfo info{entry.baseName(), {"test1", "test2", "test3"}, {nullptr}};
-                auto new_parent = new CProjectTypeItem(info, current_parent);
+                SProjectTypeInfo info;
+                QDir d(entry.absoluteFilePath(), "", QDir::Name);
+                for (QFileInfo file : d.entryInfoList(QStringList() << "*.xml", QDir::NoDotAndDotDot | QDir::Files)) {
+                    CProjectTypeXmlFileReader reader(file.absoluteFilePath());
+                    if (reader.parse()) {
+                        info = reader.getProjectTypeInfo();
+                        break;
+                    }
+                }
 
-                current_parent->appendChild(new_parent);
-                entries.push_back(qMakePair(entry, new_parent));
-            }
-
-            for (QFileInfo file : directory.entryInfoList(QStringList() << "*.xml", QDir::NoDotAndDotDot | QDir::Files)) {
+                info._name = entry.baseName();
+                auto next_parent = new CProjectTypeItem(info, current_parent);
+                current_parent->appendChild(next_parent);
+                entries.push_back(qMakePair(entry, next_parent));
             }
         }
     }
-
     return rootItem;
 }
